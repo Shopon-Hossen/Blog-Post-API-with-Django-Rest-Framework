0223e4f9bceb83fa9469195179bed0fd737f9ee0
Shopon Hossen <extremeediting572@gmail.com>
2025-03-04 15:13:24 +0600

    update settings.py for push

--- blog_post/settings.py
+++ blog_post/settings.py
@@ -133,11 +133,11 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20  # TODO: Adjust if needed.
+    'PAGE_SIZE': 10  # TODO: Adjust if needed.
 }
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),  # TODO: Adjust if needed.
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),  # TODO: Adjust if needed.
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),  # TODO: Adjust if needed.
 }
